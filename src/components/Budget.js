5e1b370d7d737fa7a75104d59e082bcddc94fbc8
Jose Rojas <jose.rojas.1@hotmail.com>
2023-04-28 04:09:59 -0400

    Task 1: Enhanced Budget Allocation Application

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,41 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget,dispatch  } = useContext(AppContext);
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={budget}
+                    style={{ marginLeft: '2rem' , size: 10}}
+                    step='10'
+                    max='20000'
+                    onChange = {(event) => 
+                        { 
+                            
+                        dispatch({ type: 'SET_BUDGET', payload: (event.target.value) });
+                        console.log(budget);
+                            /*if(budget > (event.target.value)) {
+                                alert("Out of funds");
+                            }else{
+                                dispatch({ type: 'SET_BUDGET', payload: budget });
+                            }  */
+                        }    
+                    }>
+                </input>
+            </span>
         </div>
     );
 };
 export default Budget;
+
+   /* if(action === "Set") {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budget,
+        });
+*/
